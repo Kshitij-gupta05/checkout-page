8ffb3d991c60286d56145b871e9695fd9a61b5d2
kshitij_gupta05 <kshitij.gupta6350@gmail.com>
2023-08-29 08:53:40 +0530

    payment gateway done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import Success from './components/Success'
 import Cancel from './components/Cancel'
+import Test from './components/Test'
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={<Home/>}/>
+          <Route path="/" element={<Test/>}/>
+          <Route path="/home" element={<Home/>}/>
           <Route path="/success" element={<Success/>}/>
           <Route path="/cancel" element={<Cancel/>}/>
       </Routes>
